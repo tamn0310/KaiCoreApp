548876bcdbaf6fd2aa53f358ce48efa95410431c
Tam <tamn0310@gmail.com>
2019-10-16 21:50:20 +0700

    commit 3 - created struct clientJS for web

---
+++ KaiCoreApp.Web/wwwroot/app/shared/kai.js
@@ -0,0 +1,126 @@
+﻿var kai = {
+    configs: {
+        pageSize: 10,
+        pageIndex: 1
+    },
+    notify: function (message, type) {
+        $.notify(message, {
+            clickToHide: true,
+            autoHide: true,
+            autoHideDelay: 5000,
+            arrowShow: true,
+            arrowSize: 5,
+            position: '...',
+            elementPosition: 'top right',
+            globalPosition: 'top right',
+            style: 'boostrap',
+            className: type,
+            showAnimation: 'slideDown',
+            showDuration: 400,
+            hideAnimation: 'slideUp',
+            hideDuration: 200,
+            gap: 2
+        });
+    },
+    confirm: function (message, okCallback) {
+        bootbox.confirm({
+            message: message,
+            buttons: {
+                confirm: {
+                    label: 'Đồng ý',
+                    className: 'btn-success'
+                },
+                cancel: {
+                    label: 'Hủy',
+                    className: 'btn-danger'
+                }
+            },
+            callback: function (result) {
+                if (result === true) {
+                    okCallback();
+                }
+            }
+        });
+    },
+    dateFormatJson: function (datetime) {
+        if (datetime == null || datetime == '')
+            return '';
+        var newDate = new Date(parseInt(datetime.substr(6)));
+        var month = newDate.getMonth() + 1;
+        var day = newDate.getDate();
+        var year = newDate.getFullYear();
+        var hh = newDate.getHours();
+        var mm = newDate.getMinutes();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        return day + "/" + month + "/" + year;
+    },
+    dateTimeFormatJson: function (datetime) {
+        if (datetime == null || datetime == '')
+            return '';
+        var newDate = new Date(parseInt(datetime.substr(6)));
+        var month = newDate.getMonth() + 1;
+        var day = newDate.getDate();
+        var year = newDate.getFullYear();
+        var hh = newDate.getHours();
+        var mm = newDate.getMinutes();
+        var ss = newDate.getSeconds();
+        if (month < 10)
+            month = "0" + month;
+        if (day < 10)
+            day = "0" + day;
+        if (hh < 10)
+            hh = "0" + hh;
+        if (mm < 10)
+            mm = "0" + mm;
+        if (ss < 10)
+            ss = "0" + ss;
+        return day + "/" + month + "/" + year + "" + hh + ":" + mm + ":" + ss;
+    },
+    startLoading: function () {
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading').removeClass('hide');
+    },
+    stopLoading: function () {
+        if ($('.dv-loading').length > 0)
+            $('.dv-loading').removeClass('hide')
+                .addClass('hide');
+    },
+    getStatus: function (status) {
+        if (status == 0)
+            return '<span class="badge bg-green">Kích hoạt</span>';
+        else
+            return '<span class="badge bg-red">Khóa</span>';
+    },
+    formatNumber: function (number, precision) {
+        if (!isFinite(number)) {
+            return number.toString();
+        }
+
+        var a = number.toFixed(precision).split('.');
+        a[0] = a[0].replace(/\d(?=(\d{3})+$)/g, '$&,');
+        return a.join('.');
+    },
+    unflattern: function (arr) {
+        var map = {};
+        var roots = [];
+        for (var i = 0; i < arr.length; i += 1) {
+            var node = arr[i];
+            node.children = [];
+            map[node.Id] = i;
+            if (node.ParentId != null) {
+                arr[map[node.ParentId]].children.push(node);
+            }
+            else {
+                roots.push(node);
+            }
+        }
+        return roots;
+    }
+}
